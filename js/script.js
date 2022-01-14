bea69ebbddf01bacc472cca766324d8fa2cfed9f
MatteoFilippini <matteofilippini8@gmail.com>
2022-01-14 15:08:57 +0100

    asked word

--- js/script.js
+++ js/script.js
@@ -3,8 +3,11 @@ Chiedere all’utente di inserire una parola
 Creare una funzione per capire se la parola inserita è palindroma
 */
 
-// CHIEDO LòA PAROLA ALL'UTENTE
+// CHIEDO LA PAROLA ALL'UTENTE
+const word = prompt('inserisci una parola').trim();
+console.log(word.length);
 // CONTROLLO SE E PALINDROMA
     // PASSO TUTTO L'ARRAY
     // SE IL VALORE DELLA PRIMA CELLA E UGUALE ALL ULTIMA ALLORA E PALINDROMA
 // STAMPO
+
