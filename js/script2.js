cac364265f5b229b5052b712a6a8ee706218288c
MatteoFilippini <matteofilippini8@gmail.com>
2022-01-14 17:38:06 +0100

    main

--- js/script2.js
+++ js/script2.js
@@ -11,23 +11,63 @@ Dichiariamo chi ha vinto.
 const num = document.getElementById('num');
 const choice = document.getElementById('choice');
 const buttonElement = document.querySelector('button');
-const numRandom = document.getElementById('numRandom');
 const numUser = document.getElementById('numUser');
+const numRandom = document.getElementById('numRandom');
+const vintoELement = document.getElementById('vinto');
+
 
 buttonElement.addEventListener('click', function () {
-    const numValue = num.value;
+    // prendo il numero e la scelta che inserisce l'utente
+    const numValue = parseInt(num.value);
     const choiceValue = choice.value;
-    console.log("numero utente", numValue);
-    console.log(choiceValue);
+    // evoco la funzione che mi genera un numero casuale
     random()
+    // stampo il numero dell utente e del computer
     numUser.innerText = `utente: ${numValue}`;
+    numRandom.innerText = `computer: ${numRando}`;
+    // evoco la funzione che somma i 2 numeri
+    sum(numValue, numRando);
+    // evoco la funzione che mi dice se la somma e pari o dispari
+    check(sommo);
+    // evoco la funzione che mi dice chi ha vinto
+    vittoria(choiceValue, tipo);
+    // stampo chi ha vinto
+    vintoELement.innerText = `${risultato}`;
 })
 
 // GENERO UN NUMERO CASUALE
 function random() {
-    const numRand = Math.floor(Math.random() * 5) + 1;
-    console.log("numero random", numRand);
-    numRandom.innerText = `computer: ${numRand}`;
-    return numRand;
+    numRando = Math.floor(Math.random() * 5) + 1;
+
+    return numRando;
 }
+
+// SOMMO I NUMERI
+function sum(num1, num2) {
+    sommo = parseInt(num1 + num2);
+
+    return sommo;
+}
+
+// CONTROLLO SE LA SOMMA E PARI O DISPARI
+function check(qualcosa) {
+    tipo = "pari";
+    if (qualcosa % 2 !== 0) {
+        tipo = "dispari";
+    }
+
+    return tipo;
+}
+
+//CHI HA VINTO?
+function vittoria(scelta, tipo) {
+    risultato = "UTENTE VINCE"
+    if (scelta !== tipo) {
+        risultato = "COMPUTER VINCE"
+    }
+
+    return risultato;
+}
+
+
 
