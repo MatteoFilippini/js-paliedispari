028bfb380b377e9b8a1150edf8897e9a3278dc3a
MatteoFilippini <matteofilippini8@gmail.com>
2022-01-14 21:17:21 +0100

    asked and printed word

--- js/script.js
+++ js/script.js
@@ -4,32 +4,13 @@ Creare una funzione per capire se la parola inserita è palindroma
 */
 
 // PRENDO LA PAROLA INSERITA
-const userWorld = document.getElementById('world');
+const userWorld = document.getElementById('word');
+const printWorld = document.getElementById('printWord');;
 const btn = document.querySelector('button');
 
 btn.addEventListener('click', function () {
-    const parola = [...userWorld.value];
-    // const parola = 'ossa';
-    let carattere;
-    let parolaInversa = '';
-
-    let i = parola.length - 1;
-
-    while (i >= 0) {
-        carattere = parola[i];
-        parolaInversa += carattere;
-        console.log(carattere);
-        i--;
-    }
-
-    console.log(parolaInversa);
-
-    if (parola == parolaInversa) {
-        console.log('la parola è palindroma');
-    } else {
-        console.log('la parola non è palindroma');
-    }
-
+    const userWordValue = userWord.value;
+    printWord.innerText = userWordValue;
 
 })
 
