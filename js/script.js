a72a8c8ceeeeac65bdbb02908b005634a837a681
MatteoFilippini <matteofilippini8@gmail.com>
2022-01-14 15:06:35 +0100

    created road map

---
+++ js/script.js
@@ -0,0 +1,10 @@
+/*
+Chiedere all’utente di inserire una parola
+Creare una funzione per capire se la parola inserita è palindroma
+*/
+
+// CHIEDO LòA PAROLA ALL'UTENTE
+// CONTROLLO SE E PALINDROMA
+    // PASSO TUTTO L'ARRAY
+    // SE IL VALORE DELLA PRIMA CELLA E UGUALE ALL ULTIMA ALLORA E PALINDROMA
+// STAMPO
